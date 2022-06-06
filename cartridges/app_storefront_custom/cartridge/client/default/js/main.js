224fb82c61a5afb48bb6ee1bc150652c722dbdb1
DamarisGhetes <damaris.ghetes@student.usv.ro>
2022-06-06 14:23:29 +0300

    in global.scss

--- cartridges/app_storefront_custom/cartridge/client/default/js/main.js
+++ cartridges/app_storefront_custom/cartridge/client/default/js/main.js
@@ -6,10 +6,10 @@ $(document).ready(function () {
     //processInclude(require('./scss/cart.scss'));
     processInclude(require('./components/test'));
 });
-/*
+
 $(document).ready(function(){
     $(".click-promo").on("click", function(){
         $("#enter-promo").show();
         $("#click-promo-id").hide();
     })
-})*/
+})
